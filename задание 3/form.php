64a4b5520991cb29aba2529386b4797a20557dbe
SamiraShigalugova <144467366+SamiraShigalugova@users.noreply.github.com>
2024-03-17 12:33:56 +0300

    Update form.php

--- задание 3/form.php
+++ задание 3/form.php
@@ -1,5 +1,12 @@
 <form action="" method="POST">
   <input name="fio" />
+  <input name="tel" />
+  <input name="email" />
+  <input type="radio" name="gender" value="male"> Мужской  
+  <input type="radio" name="gender" value="female"> Женский<br>    
+  <textarea name="bio"></textarea>    
+  <input type="checkbox" name="contract" value="1"> С контрактом ознакомлен(а)<br>    
+  <input type="submit" value="Сохранить" />
   <select name="year">
     <?php 
     for ($i = 1922; $i <= 2022; $i++) {
