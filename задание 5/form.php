147f88c5764b7e3140cc1784c158666487346835
SamiraShigalugova <144467366+SamiraShigalugova@users.noreply.github.com>
2024-04-22 18:36:44 +0300

    Update form.php

--- задание 5/form.php
+++ задание 5/form.php
@@ -1,14 +1,17 @@
-<html>
-  <head>
-  <head>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Контактная форма</title>
-    <link rel="stylesheet" href="styles.css">
+    <title>Форма</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
-  <body>
 
+<body>
 <?php
 if (!empty($messages)) {
   print('<div id="messages">');
@@ -18,9 +21,6 @@ if (!empty($messages)) {
   }
   print('</div>');
 }
-
-// Далее выводим форму отмечая элементы с ошибками классом error
-// и задавая начальные значения элементов ранее сохраненными.
 ?>
 
     <form action="" method="POST">
