f4028991057c82734400d62a81c9a6a01f9d930e
SamiraShigalugova <144467366+SamiraShigalugova@users.noreply.github.com>
2024-05-06 19:46:04 +0300

    Update edit_user.php

--- задание 6/edit_user.php
+++ задание 6/edit_user.php
@@ -63,6 +63,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['delete'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Редактирование пользователя</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Редактирование пользователя</h1>
