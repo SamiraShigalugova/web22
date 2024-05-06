e789d9e2eaf2f06b9b69964ee089ee9f1e76e7ad
SamiraShigalugova <144467366+SamiraShigalugova@users.noreply.github.com>
2024-05-06 19:36:03 +0300

    Update delete_user.php

--- задание 6/delete_user.php
+++ задание 6/delete_user.php
@@ -2,9 +2,12 @@
 
 // Проверка наличия идентификатора пользователя в запросе
 if (!isset($_POST['id'])) {
+    // Если идентификатор пользователя не передан, перенаправляем обратно на страницу админа
     header("Location: admin.php");
     exit();
 }
+
+// Получаем идентификатор пользователя из запроса
 $user_id = $_POST['id'];
 
 // Подключение к базе данных
@@ -12,17 +15,35 @@ try {
     $db = new PDO('mysql:host=localhost;dbname=u67419', 'u67419', '8693464', array(PDO::ATTR_PERSISTENT => true));
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
+    // Если не удалось подключиться к базе данных, выводим сообщение об ошибке
     echo "Ошибка подключения к базе данных: " . $e->getMessage();
     exit();
 }
 
-// Удаляем пользователя из базы данных
+// Удаляем связанные записи из таблицы application_languages
 try {
-    $stmt = $db->prepare("DELETE FROM application WHERE id = ?");
+    // Подготавливаем запрос на удаление
+    $stmt = $db->prepare("DELETE FROM application_languages WHERE id_app = ?");
+    // Выполняем запрос с передачей идентификатора пользователя в качестве параметра
     $stmt->execute([$user_id]);
+} catch (PDOException $e) {
+    // Если произошла ошибка при удалении связанных записей, выводим сообщение об ошибке
+    echo "Ошибка удаления связанных записей: " . $e->getMessage();
+    exit();
+}
+
+// Удаляем пользователя из таблицы application
+try {
+    // Подготавливаем запрос на удаление
+    $stmt = $db->prepare("DELETE FROM application WHERE id = ?");
+    // Выполняем запрос с передачей идентификатора пользователя в качестве параметра
+    $stmt->execute([$user_id]);
+    
+    // После удаления перенаправляем обратно на страницу админа
     header("Location: admin.php");
     exit();
 } catch (PDOException $e) {
+    // Если произошла ошибка при удалении пользователя, выводим сообщение об ошибке
     echo "Ошибка удаления пользователя: " . $e->getMessage();
     exit();
 }
